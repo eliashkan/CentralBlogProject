88cec2dd6733a8bfed46ef23c2b6c2eca93b90ef
eliashkan <khan.elias@gmail.com>
2019-12-09 11:33:16 +0100

    home servlet doGet + HttpSession

--- src/main/java/be/intecbrussel/centralblogproject/webcomponents/CentralCentralServlet.java
+++ src/main/java/be/intecbrussel/centralblogproject/webcomponents/CentralCentralServlet.java
@@ -1,4 +1,20 @@
 package be.intecbrussel.centralblogproject.webcomponents;
 
-public class CentralCentralServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class CentralCentralServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Get the cookie from the client
+        HttpSession session = req.getSession();
+    }
 }
