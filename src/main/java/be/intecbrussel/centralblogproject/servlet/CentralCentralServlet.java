3ff072f3fc5299892c7d08af4b04f43c90bf58d9
eliashkan <khan.elias@gmail.com>
2019-12-09 14:10:10 +0100

    session listener implementation to count the number of connected sessions. Will it work for just one page or for the global webapp?!

--- src/main/java/be/intecbrussel/centralblogproject/servlet/CentralCentralServlet.java
+++ src/main/java/be/intecbrussel/centralblogproject/servlet/CentralCentralServlet.java
@@ -8,8 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+// Show first 6 blogposts on homepage + number of visitors
 @WebServlet("/home")
 public class CentralCentralServlet extends HttpServlet {
+
+    // Global variable for this servlet to keep track of number of users (guests + authors) currently on page
+    int currentAmountOfUsers;
+
+    @Override
+    public void init() throws ServletException {
+        currentAmountOfUsers++;
+    }
+
+    @Override
+    public void destroy() {
+        currentAmountOfUsers--;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
