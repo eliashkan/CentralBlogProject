07f2192b28eabe8900e626931091bf53f7adb70b
eliashkan <khan.elias@gmail.com>
2019-12-13 11:10:58 +0100

    ignore pom.xml WHO DELETED MY WAR

--- src/main/java/be/intecbrussel/centralblogproject/service/RegistrationLoginServicesImpl.java
+++ src/main/java/be/intecbrussel/centralblogproject/service/RegistrationLoginServicesImpl.java
@@ -1,10 +1,28 @@
 package be.intecbrussel.centralblogproject.service;
 
+import be.intecbrussel.centralblogproject.connection.EntityManagerFactoryProvider;
+import be.intecbrussel.centralblogproject.model.User;
+
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
 public class RegistrationLoginServicesImpl implements RegistrationLoginServices {
+
     @Override
-    public boolean checkIfUsernameExistsInDB() {
-        System.out.println("test");
-        return false;
+    public boolean checkIfUsernameExistsInDB(String username) {
+        TypedQuery<User> query = EntityManagerFactoryProvider.getEM().createQuery(
+                "SELECT u FROM User u WHERE u.userName=?1",
+                User.class
+        );
+        query.setParameter(1, username);
+        User user;
+        try {
+            user = query.getSingleResult();
+        } catch (NoResultException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
