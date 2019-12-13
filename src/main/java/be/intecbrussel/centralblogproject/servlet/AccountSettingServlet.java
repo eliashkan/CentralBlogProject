abe61e61e056e7a2f87e0833d6f6780de78d60d1
vincenth1985 <Vincenth1985@gmail.com>
2019-12-13 01:52:35 +0100

    Working on staylogged metehode.Making a query for searching User that match username and password and if exist put the user fields in session.setAttr

--- src/main/java/be/intecbrussel/centralblogproject/servlet/AccountSettingServlet.java
+++ src/main/java/be/intecbrussel/centralblogproject/servlet/AccountSettingServlet.java
@@ -1,28 +1,32 @@
 package be.intecbrussel.centralblogproject.servlet;
 
+import be.intecbrussel.centralblogproject.service.AuthorServices;
+import be.intecbrussel.centralblogproject.service.AuthorServicesImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
-
-@WebServlet(value = "/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet(value = "/account")
+public class AccountSettingServlet extends HttpServlet {
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.getRequestDispatcher("resources/BlogAppFrontEnd/pages/login/login.jsp").forward(req, resp);
-
+        req.getRequestDispatcher("WEB-INF/pages/user/account.jsp").forward(req, resp);
 
     }
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 
     }
 }
