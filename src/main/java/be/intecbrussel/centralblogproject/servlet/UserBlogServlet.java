6d1112c1c58687576f2e603937929099a3dd4534
vincenth1985 <Vincenth1985@gmail.com>
2019-12-15 01:02:48 +0100

    Login Logic Ok.If user is logged he dont have to login back.I write a comin the UserBlogServlet. P.S(I use jstl for using forloops)D'ont forget to add dependency if you check out.I do not push the pom.xml

--- src/main/java/be/intecbrussel/centralblogproject/servlet/UserBlogServlet.java
+++ src/main/java/be/intecbrussel/centralblogproject/servlet/UserBlogServlet.java
@@ -23,11 +23,19 @@ public class UserBlogServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        if (req.getSession().isNew()) {
+        /*
+         * LoggedUser is a session attribute from User Object and if it is null, it means no user is logged already
+         * ,because the attribute is made when the isPasswordMatchingUsername() method gives true
+         * The only way to know if a user realy exist is when the doPost from /myblog is called by /login (isPasswordMatchingUsername())
+         * If the doPost method is called we have a User and the session is also open by the way
+         * That's why the dubbel check here
+         */
+
+        if (req.getSession().isNew() || req.getSession().getAttribute("loggedUser") == null) {
+
             req.getRequestDispatcher("WEB-INF/pages/login/login.jsp").forward(req, resp);
 
-        }
-        doPost(req, resp);
+        } else doPost(req, resp);
 
 
     }
@@ -37,18 +45,12 @@ public class UserBlogServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-        //For Testing
-        PrintWriter out = resp.getWriter();
-
         //Casting session attribute to User after redirecting from the login page.I need the User for pull the posts from User.
         User user = (User) req.getSession().getAttribute("loggedUser");
 
         //Getting the posts and put them in the session attribute
-
         req.getSession().setAttribute("postsFromUser", new VisitorServicesImpl().getPostsByAuthor(user.getUserId()));
-
         req.getRequestDispatcher("WEB-INF/pages/user/user.jsp").forward(req, resp);
-
 
 
     }
